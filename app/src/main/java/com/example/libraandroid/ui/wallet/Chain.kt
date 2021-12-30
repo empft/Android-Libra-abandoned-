364c91dd689d34eadf39f2e775bb1493ffe6f6e3
stevealexrs <aperturesciinn@gmail.com>
2021-12-30 20:41:08 +0000

    Refactored Chain and introduced common property

--- app/src/main/java/com/example/libraandroid/ui/wallet/Chain.kt
+++ app/src/main/java/com/example/libraandroid/ui/wallet/Chain.kt
@@ -3,12 +3,14 @@ package com.example.libraandroid.ui.wallet
 sealed interface Chain {
     data class Celo(
         val id: Int,
-        val chainContext: ChainContext? = null
+        override val chainContext: ChainContext? = null
     ): Chain
     data class Diem(
         val id: Int,
-        val chainContext: ChainContext? = null
+        override val chainContext: ChainContext? = null
     ): Chain
+
+    val chainContext: ChainContext?
 }
 
 data class ChainContext(
